2aae775958720e94bddf614ab22b4d4c40967f3b
pallav2712 <pallavsharma1994@gmail.com>
2026-04-23 02:12:09 +0530

    cleaned the clutter

--- app/routers/stats.py
+++ app/routers/stats.py
@@ -2,9 +2,7 @@ from fastapi import APIRouter, status, HTTPException
 import psutil
 
 
-
 router = APIRouter()
-
 
 
 @router.get("/stats", status_code=status.HTTP_200_OK)
@@ -13,7 +11,7 @@ def specifications():
     freq = round(psutil.cpu_freq(percpu=False).max/1000,1)
 
     disc_mountpoints = [item.mountpoint for item in psutil.disk_partitions()]
-    #total_disc_size = [int(psutil.disk_usage(item.mountpoint).total / (1024**3)) for item in disc_tup]
+    
     total_disks_size = []
     
     for item in disc_mountpoints:
@@ -24,9 +22,7 @@ def specifications():
         except Exception as e:
                 print(f"Error on {item}: {e}")
             
-
-    
-    return{
+    return {
                 "cpu": {
                         "cores": psutil.cpu_count(logical=False),
                         "threads": psutil.cpu_count(),       
@@ -40,36 +36,3 @@ def specifications():
 
 
 
-@router.get("/usage", status_code=status.HTTP_200_OK)
-def usage():
-     
-     freq = round(psutil.cpu_freq(percpu=False).current/1000,1)
-
-     disc_mountpoints = [item.mountpoint for item in psutil.disk_partitions()]
-    
-     disks = []
-    
-     for item in disc_mountpoints:
-        try:
-            if int(psutil.disk_usage(item).total / (1024**3)) != 0:
-                disks.append(
-                                {"allocated" : int(psutil.disk_usage(item).used / (1024**3)),
-                                        "free": int(psutil.disk_usage(item).free / (1024**3))}
-                                )
-        
-        except Exception as e:
-                print(f"Error on {item}: {e}")
-
-     return {
-                "cpu":{
-                        "frequency": freq,
-                        "percentage": psutil.cpu_percent(interval=1)
-	        },
-                
-                "ram": [ 
-		        { "allocated": psutil.virtual_memory().used, 
-                                "free": psutil.virtual_memory().free}
-                ],
-                
-                "disks": disks           
-        }
